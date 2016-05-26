eb3be97db45cbed93d1cd6f0310b08009b99b0ce
Bondar Kirill <Drix87@gmail.com>
2016-05-26 23:36:54 +0300

    Дополнение к заданию 2

--- build/js/check.js
+++ build/js/check.js
@@ -1,4 +1,15 @@
 function getMessage(a,b) {
+
+  function multiply(nameArray){
+    var arrLength = nameArray.length;
+    var result = 1;
+
+    for (var i = 0; i < arrLength; i++) {
+      result *= nameArray[i];
+    }
+    return result;
+  }
+
   if(typeof a === 'boolean'){
     if (a) {
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
@@ -8,7 +19,7 @@ function getMessage(a,b) {
   }
 
   if (typeof a === 'number') {
-    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' аттрибутов';
   }
 
    if (Array.isArray(a)) {
@@ -22,19 +33,7 @@ function getMessage(a,b) {
     return 'Количество красных точек во всех строчках изображения: ' + sum;
   }
 
-
-
-    function multiply(nameArray){
-      var arrLength = nameArray.length;
-      var result = 1;
-
-      for (var i = 0; i < arrLength; i++) {
-        result *= nameArray[i];
-      }
-      return result;
-    }
-
-  if (typeof a === 'object' && typeof b === 'object') {
+  if (Array.isArray(a) && Array.isArray(b)) {
 
     var aResult = multiply(a);
     var bResult = multiply(b);
