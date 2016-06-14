0c7f2fc6a68bec6b41e71102ae294fc6de525642
Bondar Kirill <Drix87@gmail.com>
2016-06-14 17:29:52 +0300

    module5-task1

--- src/pictures.js
+++ src/pictures.js
@@ -4,6 +4,12 @@ var picturesContainer = document.querySelector('.pictures');
 var templateElement = document.querySelector('template');
 var elementToClone;
 
+var PICTURES_LOAD_URL = 'https://o0.github.io/assets/json/pictures.json';
+
+var ACTIVE_FILTER_CLASSNAME = 'picture-filter-active';
+
+pictures = [];
+
 if ('content' in templateElement) {
   elementToClone = templateElement.content.querySelector('.picture');
 } else {
@@ -46,9 +52,95 @@ var getPictureElement = function(data, container) {
   return element;
 };
 
-window.pictures.forEach(function(picture) {
-  getPictureElement(picture, picturesContainer);
+
+var getPictures = function(callback) {
+  // Загрузите данные из файла //o0.github.io/assets/json/pictures.json по XMLHttpRequest.
+  var xhr = new XMLHttpRequest();
+
+  // Когда загрузка закончится, покажите список фотографий, как в предыдущем задании.
+  xhr.onload = function(evt) {
+    // console.log(evt.target)
+    var loadedData = JSON.parse(evt.target.response);
+    callback(loadedData);
+  };
+
+  // Пока длится загрузка файла, покажите прелоадер, добавив класс .pictures-loading блоку .pictures.
+  xhr.onprogress = function() {
+    picturesContainer.classList.add('pictures-loading');
+  };
+
+  // Если загрузка закончится неудачно (ошибкой сервера или таймаутом), покажите предупреждение об ошибке, добавив блоку .pictures класс pictures-failure.
+  if (xhr.status !== 200) {
+    picturesContainer.classList.add('pictures-failure');
+  }
+
+  xhr.open('GET', PICTURES_LOAD_URL);
+  xhr.send();
+};
+
+
+
+
+
+
+var renderPictures = function(pictures) {
+  picturesContainer.innerHTML = '';
+
+  pictures.forEach(function(picture) {
+    getPictureElement(picture, picturesContainer);
+  });
+};
+
+var getFilteredPictures = function(pictures, filter) {
+  var picturesToFilter = pictures.slice(0);
+
+  switch (filter) {
+    case 'expensive-first':
+      picturesToFilter.sort(function(a, b) {
+        return a.price - b.price;
+      });
+      break;
+  }
+
+  return picturesToFilter;
+};
+
+var setFilterEnabled = function(filter) {
+  var filteredPictures = getFilteredPictures(pictures, filter);
+  renderPictures(filteredPictures);
+
+  var activeFilter = filters.querySelector('.' + ACTIVE_FILTER_CLASSNAME);
+  if (activeFilter) {
+    activeFilter.classList.remove(ACTIVE_FILTER_CLASSNAME);
+  }
+  var filterToActivate = document.getElementById(filter);
+  filterToActivate.classList.add(ACTIVE_FILTER_CLASSNAME);
+};
+
+// Напишите обработчики событий для фильтров, так, чтобы они сортировали загруженный список фотографий следующим образом:
+var setFiltrationEnabled = function() {
+  var filtersItems = filters.querySelectorAll('.filters-item');
+  for (var i = 0; i < filtersItems.length; i++) {
+    filtersItems[i].onclick = function() {
+      setFilterEnabled(this.id);
+    };
+  }
+};
+
+  // Популярные — список фотографий, в том виде, в котором он был загружен
+
+  // Новые — список фотографий, сделанных за последние четыре дня, отсортированные по убыванию даты (поле date).
+
+  // Обсуждаемые — отсортированные по убыванию количества комментариев (поле comments)
+
+// Не забудьте, что при любом исходе загрузки (успех, ошибка, таймаут), нужно прятать прелоадер
+
+getPictures(function(loadedPictures) {
+  pictures = loadedPictures;
+  setFiltrationEnabled();
+  renderPictures(pictures);
 });
+
 
 
 // Отображает блок с фильтрами.
