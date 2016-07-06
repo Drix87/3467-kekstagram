d61a5df7717052a992ea0f7d6c1a544ade87be28
Bondar Kirill <Drix87@gmail.com>
2016-07-06 11:10:49 +0300

    module9-task3

--- src/Photo.js
+++ src/Photo.js
@@ -3,19 +3,15 @@
 var getPictureElement = require('./render-image');
 
 var Photo = function(data, container, array) {
-  var self = this;
 
   this.data = data;
   this.element = getPictureElement(this.data, container, array);
   this.index = array.indexOf(data);
-
-  this.remove = function() {
-    self.element.removeEventListener('click', this.onPictureClick);
-    self.element.parentNode.removeChild(this.element);
-  };
-
-  this.element.addEventListener('click', this.onPictureClick);
   container.appendChild(this.element);
+};
+
+Photo.prototype.remove = function() {
+  this.element.parentNode.removeChild(this.element);
 };
 
 module.exports = Photo;
--- src/gallery.js
+++ src/gallery.js
@@ -3,91 +3,92 @@
 var cssSelectorsDictionary = require('./cssSelectorsDictionary');
 
 var Gallery = function() {
-  var self = this;
+  this.galleryOverlay = document.querySelector(cssSelectorsDictionary.galleryOverlayClassName);
+  this.galleryWrapper = document.querySelector(cssSelectorsDictionary.galleryWrapperClassName);
+  this.galleryLikes = this.galleryWrapper.querySelector(cssSelectorsDictionary.galleryLikesClassName);
+  this.galleryComments = this.galleryWrapper.querySelector(cssSelectorsDictionary.galleryCommentsClassName);
+  this.galleryImg = document.querySelector(cssSelectorsDictionary.galleryImgClassName);
+  this.galleryClose = document.querySelector(cssSelectorsDictionary.galleryCloseClassName);
 
-  self.galleryOverlay = document.querySelector(cssSelectorsDictionary.galleryOverlayClassName);
-  self.galleryWrapper = document.querySelector(cssSelectorsDictionary.galleryWrapperClassName);
-  self.galleryLikes = self.galleryWrapper.querySelector(cssSelectorsDictionary.galleryLikesClassName);
-  self.galleryComments = self.galleryWrapper.querySelector(cssSelectorsDictionary.galleryCommentsClassName);
-  self.galleryImg = document.querySelector(cssSelectorsDictionary.galleryImgClassName);
-  self.galleryClose = document.querySelector(cssSelectorsDictionary.galleryCloseClassName);
+  this.galleryPictures = [];
+  this.openedFotoIndex = null;
 
-  self.galleryPictures = [];
-  self.openedFotoIndex = null;
+  this.restoreFromHash = this.restoreFromHash.bind(this);
+  window.addEventListener('hashchange', this.restoreFromHash);
 
+  this.hideGallery = this.hideGallery.bind(this);
 
-  // Создайте функцию показа галереи. Эта функция должна показывать уже существующий в разметке блок .gallery-overlay,
-  // убирая у него класс invisible. Функция должна принимать на вход номер фотографии в галерее, с которой нужно начать показ
-  self.showGallery = function(numberPhoto) {
-    self.galleryOverlay.classList.remove('invisible');
-    self.showPicture(numberPhoto, self.galleryPictures);
-
-    // После показа, добавьте обработчики событий на блок галереи:
-    self.galleryImg.addEventListener('click', self._onPhotoClick);
-    document.addEventListener('keydown', self._onDocumentKeyDown);
-    // Закрыть галлерею по клику на Х
-    self.galleryClose.addEventListener('click', function() {
-      self.hideGallery();
-      self.openedFotoIndex = 1;
-    });
-
-  };
-  self.showPicture = function(photo, array) {
-    if (typeof photo === 'string') {
-      console.log('IT IS STRINGGGG');
-      window.location.hash = photo;
-      var findIndexOfArray = function(obj) {
-        return '#photo/' + obj.url === photo;
-      };
-      var indexArray = array.findIndex(findIndexOfArray);
-      self.openedFotoIndex = indexArray;
-      self.galleryImg.src = array[indexArray].url;
-      self.galleryLikes.innerHTML = array[indexArray].likes;
-      self.galleryComments.innerHTML = array[indexArray].comments;
-    } else if (typeof photo === 'number') {
-      self.galleryImg.src = array[photo].url;
-      self.galleryLikes.innerHTML = array[photo].likes;
-      self.galleryComments.innerHTML = array[photo].comments;
-    }
-  };
-
-  window.addEventListener('hashchange', function() {
-    self.restoreFromHash();
-  });
-
-  self.restoreFromHash = function() {
-    var arrHash = window.location.hash.match(/#photo\/(\S+)/);
-    if (arrHash !== null) {
-      self.showGallery(arrHash[0]);
-    }
-  };
-
-  self.hideGallery = function() {
-    self.galleryOverlay.classList.add('invisible');
-    window.location.hash = '';
-
-    // Удаляем обработчики
-    self.galleryImg.removeEventListener('click', self._onPhotoClick);
-    document.removeEventListener('keydown', self._onDocumentKeyDown);
-  };
-
-  self._onPhotoClick = function() {
-    var nextFoto = self.openedFotoIndex += 1;
-    window.location.hash = 'photo/' + self.galleryPictures[nextFoto].url;
-  };
-  self._onDocumentKeyDown = function(evt) {
-    if ((self.galleryOverlay.classList.contains('invisible') === false) && evt.keyCode === 27) {
-      self.hideGallery();
-      self.openedFotoIndex = 1;
-    }
-  };
-  // Создайте функцию. Она будет принимать на вход массив объектов,
-  // описывающих фотографии, и сохранять их
-  self.savedPictures = function(pictures) {
-    self.galleryPictures = pictures;
-  };
 };
 
+Gallery.prototype.showGallery = function(numberPhoto) {
+  this.galleryOverlay.classList.remove('invisible');
+  this.showPicture(numberPhoto, this.galleryPictures);
+
+  // После показа, добавьте обработчики событий на блок галереи:
+  this._onPhotoClick = this._onPhotoClick.bind(this);
+  this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
+  this.galleryImg.addEventListener('click', this._onPhotoClick);
+  document.addEventListener('keydown', this._onDocumentKeyDown);
+  // Закрыть галлерею по клику на Х
+  this.closeByX = function() {
+    this.hideGallery();
+    this.openedFotoIndex = 1;
+  };
+  this.closeByX = this.closeByX.bind(this);
+  this.galleryClose.addEventListener('click', this.closeByX);
+};
+
+Gallery.prototype.showPicture = function(photo, array) {
+  if (typeof photo === 'string') {
+    window.location.hash = photo;
+    var findIndexOfArray = function(obj) {
+      return '#photo/' + obj.url === photo;
+    };
+    var indexArray = array.findIndex(findIndexOfArray);
+    this.openedFotoIndex = indexArray;
+    this.galleryImg.src = array[indexArray].url;
+    this.galleryLikes.innerHTML = array[indexArray].likes;
+    this.galleryComments.innerHTML = array[indexArray].comments;
+  } else if (typeof photo === 'number') {
+    this.galleryImg.src = array[photo].url;
+    this.galleryLikes.innerHTML = array[photo].likes;
+    this.galleryComments.innerHTML = array[photo].comments;
+  }
+};
+
+Gallery.prototype._onPhotoClick = function() {
+  var nextFoto = this.openedFotoIndex += 1;
+  window.location.hash = 'photo/' + this.galleryPictures[nextFoto].url;
+};
+
+Gallery.prototype._onDocumentKeyDown = function(evt) {
+  if ((this.galleryOverlay.classList.contains('invisible') === false) && evt.keyCode === 27) {
+    this.hideGallery();
+    this.openedFotoIndex = 1;
+  }
+};
+
+Gallery.prototype.restoreFromHash = function() {
+  var arrHash = window.location.hash.match(/#photo\/(\S+)/);
+  if (arrHash !== null) {
+    this.showGallery(arrHash[0]);
+  }
+};
+
+Gallery.prototype.hideGallery = function() {
+  this.galleryOverlay.classList.add('invisible');
+  window.location.hash = '';
+
+  // Удаляем обработчики
+  this.galleryImg.removeEventListener('click', this._onPhotoClick);
+  document.removeEventListener('keydown', this._onDocumentKeyDown);
+};
+
+// Создайте функцию. Она будет принимать на вход массив объектов,
+// описывающих фотографии, и сохранять их
+Gallery.prototype.savedPictures = function(pictures) {
+  this.galleryPictures = pictures;
+};
 
 
 module.exports = Gallery;
